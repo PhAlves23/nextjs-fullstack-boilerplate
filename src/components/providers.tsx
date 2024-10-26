be843274714c53686443c4877ceabbd48bd0822d
PhAlves23 <ph23.alves@gmail.com>
2024-10-26 07:36:22 -0300

    chore: config toast

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,23 +1,26 @@
-"use client";
+'use client'
 
-import { ReactNode } from "react";
-import { ThemeProvider } from "./theme-provider";
+import { ReactNode } from 'react'
+import { Toaster } from 'react-hot-toast'
+
+import { ThemeProvider } from './theme-provider'
 
 interface ProviderProps {
-  children: ReactNode;
+  children: ReactNode
 }
 
-export default function Providers({ children }: ProviderProps) {
+export default function Providers({ children }: Readonly<ProviderProps>) {
   return (
     <div>
       <ThemeProvider
-        attribute="class"
-        defaultTheme="system"
+        attribute='class'
+        defaultTheme='system'
         enableSystem
         disableTransitionOnChange
       >
+        <Toaster position='bottom-right' />
         {children}
       </ThemeProvider>
     </div>
-  );
+  )
 }
